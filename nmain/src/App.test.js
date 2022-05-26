af4540dce347e48fbf57441d599668eb6b4fc22e
6204062630289 <oatnakarin29062@gmail.com>
2022-05-27 02:04:55 +0700

    commit

--- nmain/src/App.test.js
+++ nmain/src/App.test.js
@@ -1,9 +1,30 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import {fireEvent} from '@testing-library/react'
+import CostInput from './test_method';
 
 test('renders learn react link', () => {
   render(<App />);
-//  render()
-  /*const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();*/
 })
+
+test('input value is updated correctly', () => {
+  const setup = () => {
+    const utils = render(<CostInput />)
+    const input = screen.getByLabelText('cost-input')
+    return {
+      input,
+      ...utils,
+    }
+  }
+
+  const {input} = setup()
+  console.log(input)
+  fireEvent.change(input, {target: {value: '23'}})
+  expect(input.value).toBe('$234')
+  
+});
+
+
+
+
+
